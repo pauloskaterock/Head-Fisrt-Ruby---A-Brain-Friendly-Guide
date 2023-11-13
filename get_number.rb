cacf0e685e27d0b3cc0142cee246e775be78673e
pauloskaterock <pauloengenharia5@yahoo.com>
2023-11-13 13:36:54 -0300

    Atualization Game Get Number

--- get_number.rb
+++ get_number.rb
@@ -3,5 +3,24 @@
 
 puts " Welcome to 'Get my number!'"
 print "Whats your name?"
-input = gets
-puts "  welcome, #{input}"
+input = gets.chomp
+name = input.chomp
+puts "  welcome, #{name}"
+
+puts "---------------------"
+puts input.inspect 
+
+# Store a random number for the player to guess.
+puts "I've got a random number between 1 and 100."
+puts "Can you guess it?"
+target = rand(100) + 1
+
+num_guesses = 0
+
+# Track how many guesses the player has made.
+num_guesses = 0
+puts "You've got #{10 - num_guesses} guesses left."
+
+#puts guess < target
+
+guess = gets.to_i
